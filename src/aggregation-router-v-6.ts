4f898075335fc8218569eedf9665810e9a469a57
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2025-07-29 20:34:45 -0400

    Add swap detection from transfer patterns + analytics entities - Working subgraph with 79k+ transfers indexed

--- src/aggregation-router-v-6.ts
+++ src/aggregation-router-v-6.ts
@@ -18,7 +18,6 @@ import {
   OwnershipTransferred,
   Paused,
   Unpaused,
-  Swapped,
   User,
   Token,
   DailyStats
@@ -184,45 +183,3 @@ export function handleUnpaused(event: UnpausedEvent): void {
   entity.save()
 }
 
-export function handleSwapped(event: SwappedEvent): void {
-  let entity = new Swapped(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.sender = event.params.sender
-  entity.srcReceiver = event.params.srcReceiver
-  entity.dstReceiver = event.params.dstReceiver
-  entity.srcToken = event.params.srcToken
-  entity.dstToken = event.params.dstToken
-  entity.amount = event.params.amount
-  entity.amountReceived = event.params.amountReceived
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  // Set up relationships using @derivedFrom instead of arrays
-  entity.user = event.params.sender
-  entity.srcTokenInfo = event.params.srcToken
-  entity.dstTokenInfo = event.params.dstToken
-  
-  // Set up daily stats relationship
-  let dailyStats = getOrCreateDailyStats(event.block.timestamp)
-  entity.dailyStats = dailyStats.id
-
-  entity.save()
-
-  // Update analytics (mutable entities)
-  let user = getOrCreateUser(event.params.sender)
-  user.totalSwaps = user.totalSwaps.plus(BigInt.fromI32(1))
-  user.totalVolume = user.totalVolume.plus(event.params.amount)
-  if (user.firstSeen.equals(BigInt.fromI32(0))) {
-    user.firstSeen = event.block.timestamp
-  }
-  user.lastSeen = event.block.timestamp
-  user.save()
-
-  // Update daily stats
-  dailyStats.swapCount = dailyStats.swapCount.plus(BigInt.fromI32(1))
-  dailyStats.totalVolume = dailyStats.totalVolume.plus(event.params.amount)
-  dailyStats.save()
-}
